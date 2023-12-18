d5672cddfa1106c092d3084aca0bd241911035f8
Roman Elizarov <elizarov@gmail.com>
2023-12-18 09:54:28 +0300

    Part18 - simple solutions with Pick's theorem

--- src/Day18_1.kt
+++ src/Day18_1.kt
@@ -1,47 +1,23 @@
+import kotlin.math.abs
+
 fun main() {
-    val a = readInput("Day18")
-    val pts = ArrayList<P2>()
+    val input = readInput("Day18")
     val (di, dj) = RDLU_DIRS
     val dn = "RDLU"
     var ci = 0
     var cj = 0
-    fun dig() { pts += P2(ci, cj) }
-    dig()
-    for (ss in a) {
+    var sa = 0
+    var sk = 0
+    for (ss in input) {
         val (ds, ns) = ss.split(" ")
         val k = ns.toInt()
         val d = dn.indexOf(ds[0])
-        repeat(k) {
-            ci += di[d]
-            cj += dj[d]
-            dig()
-        }
+        val ni = ci + di[d] * k
+        val nj = cj + dj[d] * k
+        sa += (nj - cj) * (ni + ci)
+        sk += k
+        ci = ni
+        cj = nj
     }
-    val minI = pts.minOf { it.i }
-    val minJ = pts.minOf { it.j }
-    val maxI = pts.maxOf { it.i }
-    val maxJ = pts.maxOf { it.j }
-    val n = maxI - minI + 3
-    val m = maxJ - minJ + 3
-    val f = Array(n) { IntArray(m) }
-    for ((i, j) in pts) {
-        f[i - minI + 1][j - minJ + 1] = -1
-    }
-    val q = ArrayDeque<P2>()
-    var cnt = 0
-    fun enq(i: Int, j: Int) {
-        if (i !in 0..<n || j !in 0..<m) return
-        if (f[i][j] != 0) return
-        f[i][j] = 1
-        q += P2(i, j)
-        cnt++
-    }
-    enq(0, 0)
-    while (!q.isEmpty()) {
-        val (i, j) = q.removeFirst()
-        for (d in 0..3) {
-            enq(i + di[d], j + dj[d])
-        }
-    }
-    println(n * m - cnt)
+    println(abs(sa) / 2 + sk / 2 + 1)
 }
---
+++ src/Day18_2_fill.kt
@@ -0,0 +1,70 @@
+fun main() {
+    val a = readInput("Day18")
+    data class Move(val k: Int, val d: Int)
+    val mvs = ArrayList<Move>()
+    val pts = ArrayList<P2>()
+    val (di, dj) = RDLU_DIRS
+    var ci = 0
+    var cj = 0
+    fun dig() { pts += P2(ci, cj) }
+    dig()
+    for (ss in a) {
+        val (_, _, cc) = ss.split(" ")
+        val hex = cc.removeSurrounding("(#", ")")
+        val k = hex.dropLast(1).toInt(16)
+        val d = hex.last() - '0'
+        mvs += Move(k, d)
+        ci += di[d] * k
+        cj += dj[d] * k
+        dig()
+    }
+    check(ci == 0 && cj == 0)
+    val dis = pts.map { it.i }.distinct().sorted()
+    val djs = pts.map { it.j }.distinct().sorted()
+    data class SL(val x: Int, val k: Int)
+    val stub = listOf(SL(Int.MIN_VALUE, 0), SL(Int.MAX_VALUE, 0))
+    val ii = (stub + dis.map { SL(it, 1) } + dis.zipWithNext { a, b -> SL(a + 1, b - a - 1) }.filter { it.k > 0 }).sortedBy { it.x }
+    val jj = (stub + djs.map { SL(it, 1) } + djs.zipWithNext { a, b -> SL(a + 1, b - a - 1) }.filter { it.k > 0 }).sortedBy { it.x }
+    val n = ii.size
+    val m = jj.size
+    val f = Array(n) { IntArray(m) }
+    ci = 0
+    cj = 0
+    var pi = ii.indexOfFirst { it.x == 0 }
+    var pj = jj.indexOfFirst { it.x == 0 }
+    for ((k, d) in mvs) {
+        val ni = ci + di[d] * k
+        val nj = cj + dj[d] * k
+        while (ci != ni || cj != nj) {
+            f[pi][pj] = -1
+            pi += di[d]
+            pj += dj[d]
+            ci = ii[pi].x
+            cj = jj[pj].x
+        }
+    }
+    check(ci == 0 && cj == 0)
+    val q = ArrayDeque<P2>()
+    var cnt = 0
+    fun enq(i: Int, j: Int) {
+        if (i !in 0..<n || j !in 0..<m) return
+        if (f[i][j] != 0) return
+        f[i][j] = 1
+        q += P2(i, j)
+        cnt++
+    }
+    enq(0, 0)
+    while (!q.isEmpty()) {
+        val (i, j) = q.removeFirst()
+        for (d in 0..3) {
+            enq(i + di[d], j + dj[d])
+        }
+    }
+    var sum = 0L
+    for (i in 0..<n) for (j in 0..<m) {
+        if (f[i][j] <= 0) {
+            sum += ii[i].k.toLong() * jj[j].k
+        }
+    }
+    println(sum)
+}
